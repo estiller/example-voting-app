657a6db485407c20180e69f167fff47bd4c8be53
Eran Stiller <git@stiller.co.il>
2016-09-10 15:31:16 +0300

    Update docker-compose file and build script to v2

--- buildpushall.cmd
+++ buildpushall.cmd
@@ -1,9 +1,9 @@
 docker login
 
-docker build -t stiller/examplevotingapp_vote:v1 .\vote
-docker build -t stiller/examplevotingapp_result:v1 .\result
-docker build -t stiller/examplevotingapp_worker:v1 .\worker
+docker build -t stiller/examplevotingapp_vote:v2 .\vote
+docker build -t stiller/examplevotingapp_result:v2 .\result
+docker build -t stiller/examplevotingapp_worker:v2 .\worker
 
-docker push stiller/examplevotingapp_vote:v1
-docker push stiller/examplevotingapp_result:v1
-docker push stiller/examplevotingapp_worker:v1
+docker push stiller/examplevotingapp_vote:v2
+docker push stiller/examplevotingapp_result:v2
+docker push stiller/examplevotingapp_worker:v2
